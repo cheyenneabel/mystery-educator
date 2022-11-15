bc1a1e6027324aebec29e1b9f78953ed32498d9b
Thanaa-Jaradat <thanaa.jaradat@gmail.com>
2022-11-15 10:30:52 -0500

    fixed minor issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import App from './App';
+// import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 
