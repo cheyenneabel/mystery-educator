6729640119c88afba190c21d555988fcca44bdc4
parbesh <rasaily996@gmail.com>
2022-11-15 10:02:45 -0500

    completed

---
+++ frontend/src/Components/Aboutpage.js
@@ -0,0 +1,18 @@
+
+  import React from "react";
+
+  function Aboutpage() {
+      return (
+        
+          <div>
+              <h1> Team Javengers</h1>
+          </div>
+      
+          
+      );
+    }
+  
+    
+    
+    
+    export default Aboutpage;
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+// import './App.css';
+
+function Navbar() {
+    return (
+        <div className="App">
+            <nav>
+                <h3>logo</h3>
+                <h1> Team Javengers </h1>
+            </nav>
+            <ul>
+                <li>Parbesh Portfolio</li>
+                <li>Will Portfolio</li>
+                <li>Thana Portfolio</li>
+                <li>Tyrique Porfolio</li>
+                <li>Cheyenne Portfolio</li>
+
+
+
+            </ul>
+
+        </div>
+    );
+}
+
+
+export default Navbar;
