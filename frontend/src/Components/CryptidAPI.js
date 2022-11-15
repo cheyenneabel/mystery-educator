ac2a3688f93b2b101d0dc9ef5fad29041b62de50
Thanaa-Jaradat <thanaa.jaradat@gmail.com>
2022-11-15 14:17:09 -0500

    crypted api fetch #1

--- frontend/src/Components/CryptidAPI.js
+++ frontend/src/Components/CryptidAPI.js
@@ -5,13 +5,23 @@ const Cryptid = () => {
     const [creature, setCreature] = useState({})
 
     useEffect(() => {
-        fetch('http://localhost:8080/cryptid')
-        .then()
+        fetch('http://localhost:8080/random/cryptid')
+        .then((response) => response.json())
+        .then((json) => setCreature(json))
     })
-
+    if(creature){
+        var loading = false
+    }
     return(
         <div>
-
+            {
+                (loading ? <h3>Loading...</h3> : (
+                    <div>
+                        <h2>{creature.name}</h2>
+                        <p>{creature.description}</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
