1adab128737a4eec05b2c754eb094d4111f5d3e0
Tyrique Simpson <Tyriquesimpson@gmail.com>
2022-11-16 10:43:24 -0500

    Footer.css

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import './Footer.css'
 
 // function change_page(){
 //     window.location.href = "index.html"
@@ -10,8 +10,8 @@ const Footer = () => {
    
     return(<div>
        {/* <input type="button" value="create page" onClick={change_page}/>*/}
-       <NavLink to='/jacContact'>Contact Us</NavLink>
-        <small>&copy; javengers 2022</small>
+       <NavLink to='/jacContact'> Contact Us </NavLink>
+        <small>&copy; javengers 2022 </small>
     </div>)
     
 }
