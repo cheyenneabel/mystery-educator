40ff6e841cacbd64fd9c5e6622b018eebf3c954d
parbesh <rasaily996@gmail.com>
2022-11-17 10:03:23 -0500

    completed

--- frontend/src/PageSwitch.js
+++ frontend/src/PageSwitch.js
@@ -21,7 +21,7 @@ const PageSwitch = () => {
             <Route exact path = "/bored" component={BoredAPI} />
 
             <Route exact path = "/museum" component={MetMuseumAPI} />
-            <Route exact path = "/javengers" component={Aboutpage} />
+            <Route exact path = "/javengers" component={AboutPage} />
             {/* <Route exact path="/aboutus" component={AboutPage}/> */}
             <Route exact path = "/jacContact" component={jacContactPage} />
 
